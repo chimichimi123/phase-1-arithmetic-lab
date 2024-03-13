4a75880106cf70f984565ea869a6724969312c8f
Christopher Cantu <chrischimi02@gmail.com>
2024-03-12 19:28:28 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [10, 20, 15, 5];
+const max = Math.max(...numbers);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max,
+};
